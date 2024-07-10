c741d1b4549a6c099cd71a9e0cb542d5475d753b
kc0 <katiec2312@gmail.com>
2024-07-10 10:57:33 +0100

    test commit

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -21,7 +21,7 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">
+        <Heading fontSize="xl">
           {game.name}
           <Emoji rating={game.rating_top} />
         </Heading>
